94d631a57c63583fe709de149cb2b086c9320d99
TatyanaKozhokar <tz5799@gmail.com>
2025-06-28 23:19:01 +0300

    The Project

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static org.junit.Assert.assertEquals;
 
 public class LoginPage {
     public LoginPage(WebDriver driver) {
